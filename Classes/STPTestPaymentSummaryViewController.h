cd5c60471dfbf6887d39a1e266bd40a06ba637e7
Jack Flintermann <jack@stripe.com>
2014-10-07 17:53:07 -0700

    organization and README

---
+++ Classes/STPTestPaymentSummaryViewController.h
@@ -0,0 +1,21 @@
+//
+//  STPTestPaymentSummaryViewController.h
+//  StripeExample
+//
+//  Created by Jack Flintermann on 9/8/14.
+//  Copyright (c) 2014 Stripe. All rights reserved.
+//
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000 && defined(STRIPE_ENABLE_APPLEPAY)
+
+#import <UIKit/UIKit.h>
+#import <PassKit/PassKit.h>
+
+@interface STPTestPaymentSummaryViewController : UIViewController
+
+- (instancetype)initWithPaymentRequest:(PKPaymentRequest *)paymentRequest;
+@property(nonatomic, assign)id<PKPaymentAuthorizationViewControllerDelegate>delegate;
+
+@end
+
+#endif
