e3c5dd6fd804542150b78f1d716669334105e57c
Jack Flintermann <jack@stripe.com>
2015-04-29 13:56:42 -0400

    add xcodeproj

---
+++ ApplePayStubs/ApplePayStubs.h
@@ -0,0 +1,19 @@
+//
+//  ApplePayStubs.h
+//  ApplePayStubs
+//
+//  Created by Jack Flintermann on 4/29/15.
+//  Copyright (c) 2015 stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApplePayStubs.
+FOUNDATION_EXPORT double ApplePayStubsVersionNumber;
+
+//! Project version string for ApplePayStubs.
+FOUNDATION_EXPORT const unsigned char ApplePayStubsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplePayStubs/PublicHeader.h>
+
+
